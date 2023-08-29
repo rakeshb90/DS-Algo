d8a5a0039a317ef590ce4e98b7c8b670c6ef98bf
rakeshb90 <rkplacementb9@gmail.com>
2023-08-29 08:54:56 +0530

    array folder added

---
+++ DSA/src/Array/MaxSumSubArr.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class MaxSumSubArr {
+	// Cadane's Algorithm
+	public static int getMaxsum(int[] arr) {
+		int currSum=0,maxSum=Integer.MIN_VALUE;
+		for(int index=0;index<arr.length;index++) {
+			currSum = Math.max(currSum+arr[index], arr[index]);
+			maxSum = Math.max(maxSum, currSum);
+		}
+		return maxSum;
+	}
+	
+	public static void main(String[] args) {
+		int[] arr= {1,-2,0,9,-5,3,8,0,-6,10};
+		System.out.println("Max Sum: "+getMaxsum(arr));
+		
+	}
+
+}
